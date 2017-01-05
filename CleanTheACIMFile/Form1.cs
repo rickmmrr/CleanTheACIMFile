7397e7a84e1b1ccd45e8bcfe958b414ddf21a7ad
Rick <Rick@DESKTOP-GEC08PE>
2017-01-05 11:12:46 -0800

    more

--- CleanTheACIMFile/Form1.cs
+++ CleanTheACIMFile/Form1.cs
@@ -4,11 +4,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
+
 
 namespace CleanTheACIMFile
 {
     public partial class Form1 : Form
     {
+        
+
+
         public Form1()
         {
             InitializeComponent();
@@ -16,63 +21,89 @@ namespace CleanTheACIMFile
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string[] rawACIMFile = null;
-
             try
             {
 
-                rawACIMFile = File.ReadAllLines(@"..\..\acim-3.txt");
-            }
-            catch(Exception ex)
-            {
-                Debug.WriteLine($"Error = {ex.Message}");
-            }
 
-            //start working on the file.
 
-            ExamineACIMFile examine = new ExamineACIMFile(rawACIMFile);
 
-            List<RawDataContainer> rawDataCon = null;
-            DuplicateLineItem[] dub = examine.DetermineDuplicateLines(out rawDataCon);
+                string[] rawACIMFile = null;
 
-            try
-            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string rootPath = appSettings["TextPath"] ?? "Not Found";
+                if (rootPath == "Not Found")
+                    throw new Exception("Root text path is not set");
 
-                List<string> retList = new List<string>();
 
-                foreach (DuplicateLineItem d in dub)
+
+                try
                 {
-                    retList.Add($"{d.Count}   {d.Line}");
+
+
+                    rawACIMFile = File.ReadAllLines(rootPath + "acim-3.txt");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error = {ex.Message}");
+                }
+
+                //start working on the file.
+
+                ExamineACIMFile examine = new ExamineACIMFile(rawACIMFile);
+
+                List<RawDataContainer> rawDataCon = null;
+                DuplicateLineItem[] dub = examine.DetermineDuplicateLines(out rawDataCon);
+
+                try
+                {
+
+                    List<string> retList = new List<string>();
+
+                    foreach (DuplicateLineItem d in dub)
+                    {
+                        retList.Add($"{d.Count}   {d.Line}");
+                    }
+
+
+                    //save the file
+                    File.WriteAllLines(rootPath + "dubFile.txt", retList.ToArray());
+
+
+                    StringBuilder sb = new StringBuilder();
+                    foreach (RawDataContainer r in rawDataCon)
+                    {
+                        if (!r.ThrowOut)
+                            sb.AppendLine(r.Line);
+                    }
+
+
+                    File.WriteAllLines(rootPath + "rawDataStepOne.txt", new string[] { sb.ToString() });
+
+                }
+                catch (Exception exe)
+                {
+                    Debug.WriteLine($"Error = {exe.Message}");
                 }
 
 
-                //save the file
-                File.WriteAllLines(@"..\..\dubFile.txt", retList.ToArray());
 
 
-                StringBuilder sb = new StringBuilder();
-                foreach (RawDataContainer r in rawDataCon)
-                {
-                    if (!r.ThrowOut)
-                        sb.AppendLine(r.Line);
-                }
 
 
-                File.WriteAllLines(@"..\..\rawDataStepOne.txt", new string[] { sb.ToString() });
+
+                MessageBox.Show("Done!");
+
+
+
+
 
             }
-            catch (Exception exe)
+            catch (Exception re)
             {
-                Debug.WriteLine($"Error = {exe.Message}");
+                Debug.WriteLine($"Error = {re.Message}");
             }
 
 
-
-
-
-
-
-            MessageBox.Show("Done!");
         }
     }
 }
